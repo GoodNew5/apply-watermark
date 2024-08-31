632270d69f4e2a683aeeda86e20d71e67b31ed09
Aleksander Mosin <82818546+mosinas-dev@users.noreply.github.com>
2024-09-01 02:19:37 +0700

    обработка файлов по событиям

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import ffmpeg from 'fluent-ffmpeg'
 import fs from 'fs'
 import path from 'path'
 import mime from 'mime'
+import chockidar from 'chokidar';
+import { fileURLToPath } from 'url'
 
+
+const INPUT_DIR = './src/input' // put your video there
+const OUTPUT_DIR = './src/output' // get your processed video
+const WATERMARK_PATH = './src/watermark.png' // replace by your watermark
 
 interface VideoResolution {
   width: number
   height: number
 }
+
+const watcher = chockidar.watch(INPUT_DIR, {
+  ignored: /(^|[\/\\])\../, // ignore dotfiles
+  persistent: true,
+})
 
 async function getVideoResolution(videoPath: string): Promise<VideoResolution> {
     return new Promise((resolve, reject) => {
@@ -28,85 +39,87 @@ async function getVideoResolution(videoPath: string): Promise<VideoResolution> {
   })
 }
 
-const VIDEO_DIR = './src/input' // put your video there
-const OUTPUT_DIR = './src/output' // get your processed video
-const WATERMARK_PATH = './src/watermark.png' // replace by your watermark
+async function applyWatermark(mediaFile?: string) {
+  if (!fs.existsSync(WATERMARK_PATH)) {
+    console.error(`Watermark file not found at: ${WATERMARK_PATH}`)
 
-async function applyWatermark() {
+    return
+  }
+
   if (!fs.existsSync(OUTPUT_DIR)) {
     fs.mkdirSync(OUTPUT_DIR, { recursive: true })
   }
 
-  const processVideo = async (videoFile: string) => {
-    if (!fs.existsSync(WATERMARK_PATH)) {
-      console.error(`Watermark file not found at: ${WATERMARK_PATH}`)
+  const processMediaFile = async (mediaFile: string) => {
+    const OUTPUT_PATH = path.join(
+      OUTPUT_DIR,
+      `${path.basename(mediaFile, path.extname(mediaFile))}${path.extname(mediaFile)}`,
+    )
 
-      return
-    }
+    console.log(`Processing file: ${mediaFile}`)
 
-    const VIDEO_PATH = path.join(VIDEO_DIR, videoFile)
-    const OUTPUT_PATH = path.join(OUTPUT_DIR, `${path.basename(videoFile, path.extname(videoFile))}_processed.mp4`)
-
-    console.log(`Processing video file: ${VIDEO_PATH}`)
-    console.log(`Output will be saved to: ${OUTPUT_PATH}`)
+    console.log(`Output will be saved to: ${OUTPUT_DIR}`)
+    console.log(`Output file: ${OUTPUT_PATH}`)
 
     fs.access(OUTPUT_DIR, fs.constants.W_OK, (err) => {
       if (err) {
         console.error(`Cannot write to output directory: ${OUTPUT_DIR}`)
-
         return
       }
     })
 
-    try {
-      const dimensions: VideoResolution = await getVideoResolution(VIDEO_PATH)
-      const width = dimensions?.width
-      const height = dimensions?.height
+    const dimensions: VideoResolution = await getVideoResolution(mediaFile)
+    const width = dimensions?.width
+    const height = dimensions?.height
 
-      if (!width) throw Error('failed to get video width')
+    if (!width) {
+      console.error('failed to get video width')
 
-      const getWatermarkSize = () => {
-        // Set the watermark width to be 10% of the video width
-        return Math.round((width + height) * 0.1)
-      }
-
-      const watermarkWidth = getWatermarkSize()
-      return new Promise((resolve, reject) => {
-        ffmpeg(VIDEO_PATH)
-          .input(WATERMARK_PATH)
-          .complexFilter(
-            `[1:v]scale=${watermarkWidth}:-1[wm];[0:v][wm]overlay=x='W/2-pow(-1,lt(mod(t,20),10))*((W-w)/2-10)-w/2':y='H/2-pow(-1,lt(mod(t,10),5))*((H-h)/2-10)-h/2'`,
-          )
-          .outputOptions('-movflags frag_keyframe+empty_moov')
-          .toFormat('mp4')
-          .save(OUTPUT_PATH)
-          .on('end', () => {
-            fs.unlinkSync(VIDEO_PATH);
-            resolve(`Finished processing video: ${VIDEO_PATH}`)
-          })
-          .on('error', (error) => {
-            reject(`Error processing video file: ${error}`)
-          })
-      })
-    } catch (err) {
-      console.error(err)
+      return false
     }
 
+    const watermarkWidth = Math.round((width + height) * 0.1) // Set the watermark width to be 10% of the video width
+
+    return new Promise((resolve, reject) => {
+      ffmpeg(mediaFile)
+        .input(WATERMARK_PATH)
+        .complexFilter(
+          `[1:v]scale=${watermarkWidth}:-1[wm];[0:v][wm]overlay=x='W/2-pow(-1,lt(mod(t,20),10))*((W-w)/2-10)-w/2':y='H/2-pow(-1,lt(mod(t,10),5))*((H-h)/2-10)-h/2'`,
+        )
+        .outputOptions('-movflags frag_keyframe+empty_moov')
+        .save(OUTPUT_PATH)
+        .on('end', () => {
+          console.log(`Finished processing file: ${mediaFile}`)
+          fs.unlinkSync(mediaFile)
+          resolve(`Finished processing file: ${mediaFile}`)
+        })
+        .on('error', (error) => {
+          console.error(`Error processing file: ${error}`)
+          reject(`Error processing file: ${error}`)
+        })
+    })
   }
 
-  const files = fs.readdirSync(VIDEO_DIR)
-  const videoFiles = files.filter((file) => mime.getType(file)?.startsWith('video'))
+  function checkIsMedia(file: string) {
+    return mime.getType(file)?.startsWith('video') || mime.getType(file)?.startsWith('image')
+  }
 
-  for (const videoFile of videoFiles) {
-    try {
-      await processVideo(videoFile)
-    } catch (error) {
-      console.error(error)
+  if (mediaFile) {
+    const media = checkIsMedia(mediaFile)
+
+    if (media) {
+        processMediaFile(mediaFile)
     }
   }
 }
 
-while(true) {
-  await applyWatermark()
-  setTimeout(() => {}, 2000);
-}
+console.log("Service started...")
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
+console.log(__dirname)
+
+watcher.on('add', (filePath) => {
+  console.log(`File added: ${filePath}`)
+  applyWatermark(filePath)
+})
